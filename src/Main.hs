dba596c63471471db81a07b4edab8b38de0c82b8
Even Brenden <evenbrenden@gmail.com>
2019-06-09 10:41:07 +0200

    Add functions for modifying DB over TCP

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,7 @@ import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Database.SQLite.Simple hiding (close)
 import qualified Database.SQLite.Simple as SQLite
+import Database.SQLite.Simple.Types
 import Network.Socket hiding (recv)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
@@ -58,7 +59,7 @@ handleQuery :: Connection
             -> IO ()
 handleQuery dbConn soc = do
 
-    msg <- recv soc 1025
+    msg <- recv soc 1024
 
     case msg of
         "\r\n" ->
@@ -77,8 +78,8 @@ handleQueries dbConn sock = forever $ do
     handleQuery dbConn soc
     Network.Socket.close soc
 
-main :: IO ()
-main = withSocketsDo $ do
+fingerQuery :: IO ()
+fingerQuery = withSocketsDo $ do
 
     let port = "79"
     addrinfos <- getAddrInfo
@@ -99,3 +100,68 @@ main = withSocketsDo $ do
 
     SQLite.close conn
     Network.Socket.close sock
+
+dump :: Connection -> IO ()
+dump conn = do
+
+    rows <- query_ conn allUsers
+    mapM_ print (rows :: [User])
+
+handleInsert :: Connection
+            -> Socket
+            -> IO ()
+handleInsert dbConn soc = do
+
+    msg <- recv soc 1024
+    let stripped = T.strip $ decodeUtf8 msg
+    let fields = T.split (== ',') stripped
+    if (length fields == 5) then do
+        let [username', shell', homeDirectory', realName', phone'] = fields
+        execute_ dbConn createUsers
+        maybeUser <- getUser dbConn username'
+        case maybeUser of
+            Nothing -> do
+                execute dbConn insertUser (Null, username', shell', homeDirectory', realName', phone')
+                dump dbConn
+            Just _ -> do
+                execute dbConn updateUser (shell', homeDirectory', realName', phone', username')
+                dump dbConn
+    else
+        putStrLn $ mconcat [ "Received ", show msg, ", need \"[username],[shell],[home directory],[real name],[phone]\\r\\n\"" ]
+
+handleInserts :: Connection
+              -> Socket
+              -> IO ()
+handleInserts dbConn sock = forever $ do
+
+    (soc, _) <- accept sock
+    putStrLn "Got connection, handling insert"
+
+    handleInsert dbConn soc
+    Network.Socket.close soc
+
+fingerInsert :: IO ()
+fingerInsert = withSocketsDo $ do
+
+    let port = "80"
+    addrinfos <- getAddrInfo
+        (Just (defaultHints
+            {addrFlags =
+                [AI_PASSIVE]}))
+        Nothing (Just port)
+    let serveraddr = head addrinfos
+    sock <- socket
+        (addrFamily serveraddr)
+        Stream defaultProtocol
+    Network.Socket.bind sock (addrAddress serveraddr)
+
+    listen sock 1
+    -- Only one connection open at a time
+    conn <- open "finger.db"
+    handleInserts conn sock
+
+    SQLite.close conn
+    Network.Socket.close sock
+
+main :: IO ()
+main = fingerInsert
